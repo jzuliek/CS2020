08431fb83ded321663aa73446c5eedbde783f17f
Code <code@COCTL-41300.local>
2020-01-21 11:10:46 -0800

    fix api call

--- BART-api/src/app/views/pages/mains/mains.component.ts
+++ BART-api/src/app/views/pages/mains/mains.component.ts
@@ -8,9 +8,15 @@ import { ServicesService } from 'src/app/data/services.service';
 })
 export class MainsComponent implements OnInit {
   apiURL = 'http://api.bart.gov/api/etd.aspx?cmd=etd&orig=RICH&key=Z2R9-5KQ2-9SYT-DWE9&json=y'
+  
   constructor(private dataS: ServicesService) { }
 
   ngOnInit() {
+    this.dataS.getURL(this.apiURL).subscribe(
+      x => {
+        console.log(x);
+      }
+    )
   }
 
 }
